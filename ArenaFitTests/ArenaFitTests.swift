185e459f1820aee301f37aed032fead72b125bb3
jlbvxf <jaronlbaston@gmail.com>
2025-09-09 00:36:14 -0500

    Initial Commit

---
+++ ArenaFitTests/ArenaFitTests.swift
@@ -0,0 +1,16 @@
+//
+//  ArenaFitTests.swift
+//  ArenaFitTests
+//
+//  Created by Jaron Baston on 9/9/25.
+//
+
+import Testing
+
+struct ArenaFitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
